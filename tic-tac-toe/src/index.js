affa4aece1c49d8c580583bb24d5cabd66655651
Daniel Palmieri <dpalmieri@ycp.edu>
2020-02-03 17:11:49 -0500

    changed square() to be a function component, added a calculateWinner() helper function for determining who has won

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -2,30 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square" 
-          onClick={() => this.props.onClick({value: 'X'})}
-        >
-          {this.props.value}
-        </button>
-      );
-    }
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
   
   class Board extends React.Component {
     constructor(props) {
       super(props);
         this.state = {
           squares: Array(9).fill(null),
+          xIsNext: true,
         };
     }
 
     handleCLick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
 
     renderSquare(i) {
@@ -35,7 +35,7 @@ class Square extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: '+ (this.state.xIsNext ? 'Player X' : 'Player 0');
   
       return (
         <div>
@@ -83,3 +83,23 @@ class Square extends React.Component {
     document.getElementById('root')
   );
   
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
