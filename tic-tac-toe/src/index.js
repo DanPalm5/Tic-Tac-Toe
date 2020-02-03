ab839600022bd96fbc7930d2308c2a794be7eb70
Daniel Palmieri <dpalmieri@ycp.edu>
2020-02-03 16:54:15 -0500

    added ability to add an 'x' to squares on click

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" 
+          onClick={() => this.setState({value: 'X'})}
+        >
+          {this.state.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+        this.state = {
+          squares: Array(9).fill(null),
+        };
+    }
+
     renderSquare(i) {
-      return <Square />;
+      return <Square value={this.state.squares[i]} 
+      onClick={() => this.handleCLick(i)}
+      />;
     }
   
     render() {
