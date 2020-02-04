795535db9a14cfdff466eeb30970971456a87b9e
Daniel Palmieri <dpalmieri@ycp.edu>
2020-02-03 20:46:29 -0500

    successfully added 'going back in time' (user can now restore previous moves). Game functionality is complete. Next steps will be modifying the HTML/CSS

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -46,12 +50,13 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
-      xIsNext: true
+      stepNumber: 0,
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -62,14 +67,33 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpto(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2 == 0),
     });
   }
   
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' +move :
+        'Go to game start';
+      return(
+        <li key={move}>
+        <button onClick={() => this.jumpTo(move)}>{desc}</button>        
+        </li>
+        );
+    });
 
     let status;
     if (winner) {
@@ -88,7 +112,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
