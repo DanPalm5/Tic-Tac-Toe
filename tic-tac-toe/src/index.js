c4f46d28e8e7f48cc5e0f9cb479c5043541af347
Daniel Palmieri <dpalmieri@ycp.edu>
2020-02-03 21:59:52 -0500

    added styles, background image to improve visual apperance

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -75,7 +75,7 @@ class Game extends React.Component {
   jumpto(step) {
     this.setState({
       stepNumber: step,
-      xIsNext: (step % 2 == 0),
+      xIsNext: (step % 2 === 0),
     });
   }
   
@@ -97,7 +97,7 @@ class Game extends React.Component {
 
     let status;
     if (winner) {
-      status = 'Winner: ' + winner;
+      status = '' +winner + ' wins!!!'
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
